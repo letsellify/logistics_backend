cbbf626bfe075ed0e50c8ff91b2564a1bb94c21c
Ahmadbuba <ahmadbubacherryfield@gmail.com>
2025-07-16 15:13:42 +0100

    switched to prod

--- src/main/java/com/letsellify/logistics/components/user/core/socialSignupAndLogin/config/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/letsellify/logistics/components/user/core/socialSignupAndLogin/config/OAuth2LoginSuccessHandler.java
@@ -69,14 +69,14 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
         if (appUser.getRole() == null) {
             final String token = this.tokenManager.getAccessTokenForOAuth2(securityUser);
-            response.sendRedirect("http://localhost:5173/join-as/SIGNUP?" + token);
+            response.sendRedirect("https://logistics.letsellify.com/join-as/SIGNUP?" + token);
         } else {
             final String domain = "localhost";
             final LogisticsAppSecurityToken securityToken = this.tokenManager.getToken(securityUser);
             CookieUtil.addCookie(response, domain, "access_token", securityToken.getAccessToken(), 15 * 60);
             CookieUtil.addCookie(response, domain,"refresh_token", securityToken.getRefreshToken(), 7 * 24 * 60 * 60);
             this.addSecurityHeaders(response);
-            response.sendRedirect("http://localhost:5173/homepage");
+            response.sendRedirect("https://logistics.letsellify.com/homepage");
         }
     }
 
